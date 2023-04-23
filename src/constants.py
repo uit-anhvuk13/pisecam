ee7bf94e82b3e3de14e13a315c56925852aad076
Vu Anh Tran <anvut20@gmail.com>
2023-04-24 04:15:57 +0700

    first commit - skeleton

---
+++ src/constants.py
@@ -0,0 +1,5 @@
+TIMEOUT = 1
+SERVER = '0.0.0.0'
+PORT = '3000'
+
+SECRET_KEY = 'supersecretkey'
---
+++ src/shared_queues.py
@@ -0,0 +1,5 @@
+from queue import Queue
+
+StreamConnectionQueue = Queue()
+CameraCtrlQueue = Queue()
+FrameQueue = Queue()
